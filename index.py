c959a3793bba3fb252f5b50b9b67626d93cb4d28
shubhamxcode <shubh.varshneycode@gmail.com>
2023-11-10 23:24:58 +0530

    learning python first commit msg

---
+++ index.py
@@ -0,0 +1,60 @@
+print("hello world")
+
+
+print ("hello shubham ji,\"nice to meet u\",we will meet again")
+print("shubham varshney is great")# varshney ji ka rola hai 
+#elvish bhai ke agge koi bol sakta hai kya 
+print(" aditya bhai ke agge koi bol sakta hai kya\nhey aditya bhai ")
+print("hey",6,7,sep="~",end="009\n")
+print (" aditya bhai ka system hai ")
+
+
+a=7
+b=7
+print (a+b)
+print("the type of a is " ,type(a))
+c=complex(5,7)
+d=67
+print (c-d)
+print("the type of c is ", type(c))
+list = [6,2.3],[36,8.9],["apple","bannana"]
+print(list)
+tuple =("chita,lion,panchi")
+print(tuple)
+dict2 = {"name":"sakshi", "age":18,"talent":100}
+
+print(2+3)
+print(2-3)
+print(2*3)
+print(2%3)
+print(2/3)
+print(2//3)
+print(2**4)
+n=15
+m=7
+print("the additon of",n,"and",m,"is",22)
+print("the division of ",n,"and",m,"is",2.1)
+print("the exponential of ",n,"and",m,"is",15**7)
+print("the modules of is",n,"and",m,"is",15%7)
+print("the floor division of",n,"and",m,"is",15//7)
+print("the value of ",n,"+",m,"is",n+m)
+print("the value of ",n ,"%",m,"is",n%m)
+print("the value of ",n ,"//",m,"is:",n//m)
+
+
+
+a="2"
+b=4
+print(int(a)+b)
+# implicit function
+a=6.9
+b=6
+print(a+b)
+
+
+
+print('hello world')
+print("elvish bhai ke agge koi bol sakta kya aeee elvish bhai \"hue hue sardar")
+print('''hello shubham
+varsheny ji
+hue hue sardar''')
